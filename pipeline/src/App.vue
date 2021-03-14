24b27a3de57b9a16b6e6094da99ab3edddb023ca
LiFuHai <57932634+LiFuHai1106@users.noreply.github.com>
2021-03-14 23:01:54 +0800

    pipeline组件

--- pipeline/src/App.vue
+++ pipeline/src/App.vue
@@ -1,18 +1,21 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <pipeline></pipeline>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+// import HelloWorld from "./components/HelloWorld.vue";
+import pipeline from "./components/Pipeline/index.vue";
 
 export default Vue.extend({
   name: "App",
   components: {
-    HelloWorld,
+    // HelloWorld,
+    pipeline,
   },
 });
 </script>
